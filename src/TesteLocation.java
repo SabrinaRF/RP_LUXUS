89419e92854c5c267fa22b8c693cbe94b9880d1d
JonesGamer18 <106637017+JonesGamer18@users.noreply.github.com>
2022-10-23 23:43:06 -0300

    Update TesteLocation.java

--- src/TesteLocation.java
+++ src/TesteLocation.java
@@ -1,8 +1,14 @@
 import static org.junit.Assert.assertNotEquals;
+
+import javax.print.attribute.standard.MediaSize.Other;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TesteLocation {
+    protected int x;
+    protected int y;
+
     @Test
     public void nextLocation() {
         Location location = new Location(122020, 1000);
@@ -55,4 +61,29 @@ public class TesteLocation {
 
     }
 
+    @Test
+    public void equals() {
+        Location other = new Location(5, 13);
+
+        Location otherLocation = new Location(20, 25);
+        if (other == otherLocation) {
+
+            Location resultado = other = otherLocation;
+            Location esperada = other = otherLocation;
+            assertEquals(esperada, resultado);
+        }
+
+    }
+
+    @Test
+    public void distance() {
+        Location destination = new Location(5, 10);
+        int xDist = Math.abs(destination.getX() - x);
+        int yDist = Math.abs(destination.getX() - x);
+
+        int resultado = Math.max(xDist, yDist);
+        int esperada = Math.max(xDist, yDist);
+        assertEquals(esperada, resultado);
+
+    }
 }
