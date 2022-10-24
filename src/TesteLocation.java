2c071f4cc806e59d66e03ec795d503b6f03967dd
JonesGamer18 <106637017+JonesGamer18@users.noreply.github.com>
2022-10-24 10:40:44 -0300

    Update TesteLocation.java

--- src/TesteLocation.java
+++ src/TesteLocation.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import javax.print.attribute.standard.MediaSize.Other;
 
@@ -21,6 +22,17 @@ public class TesteLocation {
     }
 
     @Test
+    public void nextLocationError() {
+        Location location = new Location(25, 40);
+        Location destino = new Location(15, 22);
+        Location locationEsperada = new Location(12, 40);
+
+        Location resultado = location.nextLocation(destino);
+
+        assertEquals(locationEsperada, resultado);
+    }
+
+    @Test
     public void getLocation() {
         Location location = new Location(5, 10);
 
@@ -33,20 +45,46 @@ public class TesteLocation {
     }
 
     @Test
+    public void getLocationError() {
+        Location location = new Location(5, 10);
+
+        Location esperada = new Location(22, 30);
+
+        Location resultado = location.getLocation();
+
+        assertEquals(esperada, resultado);
+
+    }
+
+    @Test
     public void getY() {
         Location y = new Location(0, 5);
-        Location esperada = new Location(0, 5);
+        int esperada = y.getX();
+        int resultado = y.getX();
+        assertEquals(esperada, resultado);
+    }
 
-        int resultado = y.getY();
-
+    @Test
+    public void getYError() {
+        Location y = new Location(0, 5);
+        int esperada = y.getX();
+        int resultado = y.getX();
         assertNotEquals(esperada, resultado);
     }
 
     @Test
     public void getX() {
         Location x = new Location(4, 0);
-        Location esperada = new Location(4, 0);
+        int esperada = x.getX();
+        int resultado = x.getX();
 
+        assertEquals(esperada, resultado);
+    }
+
+    @Test
+    public void getXError() {
+        Location x = new Location(4, 0);
+        int esperada = x.getX();
         int resultado = x.getX();
 
         assertNotEquals(esperada, resultado);
@@ -62,15 +100,38 @@ public class TesteLocation {
     }
 
     @Test
+    public void ToStringError() {
+        String resultado = "location";
+        String esperada = "location";
+
+        assertNotEquals(esperada, resultado);
+
+    }
+
+    @Test
     public void equals() {
-        Location other = new Location(5, 13);
+        Object other = new Object();
 
-        Location otherLocation = new Location(20, 25);
-        if (other == otherLocation) {
+        if (other instanceof Location) {
+            Location otherLocation = (Location) other;
+            int resultado = otherLocation.getX() & otherLocation.getY();
+            int esperada = otherLocation.getX() & otherLocation.getY();
+            assertNotEquals(esperada, resultado);
 
-            Location resultado = other = otherLocation;
-            Location esperada = other = otherLocation;
-            assertEquals(esperada, resultado);
+        }
+
+    }
+
+    @Test
+    public void equalsError() {
+        Location other = new Location(5, 10);
+
+        if (other instanceof Location) {
+            Location otherLocation = (Location) other;
+            int resultado = otherLocation.getX() & otherLocation.getY();
+            int esperada = otherLocation.getX() & otherLocation.getY();
+            assertNotEquals(esperada, resultado);
+
         }
 
     }
@@ -79,11 +140,23 @@ public class TesteLocation {
     public void distance() {
         Location destination = new Location(5, 10);
         int xDist = Math.abs(destination.getX() - x);
-        int yDist = Math.abs(destination.getX() - x);
+        int yDist = Math.abs(destination.getY() - y);
 
         int resultado = Math.max(xDist, yDist);
         int esperada = Math.max(xDist, yDist);
         assertEquals(esperada, resultado);
+
+    }
+
+    @Test
+    public void distanceError() {
+        Location destination = new Location(5, 10);
+        int xDist = Math.abs(destination.getX() - x);
+        int yDist = Math.abs(destination.getY() - y);
+
+        int resultado = Math.max(xDist, yDist);
+        int esperada = Math.max(xDist, yDist);
+        assertNotEquals(esperada, resultado);
 
     }
 }
