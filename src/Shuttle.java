40fdb40e7d35bb3b05fbc38e6fc9d3ce70d5c561
JonesGamer18 <106637017+JonesGamer18@users.noreply.github.com>
2022-10-29 16:18:05 -0300

    Update shutle

--- src/Shuttle.java
+++ src/Shuttle.java
@@ -2,88 +2,64 @@ import java.util.List;
 import java.util.LinkedList;
 import java.awt.Image;
 import javax.swing.ImageIcon;
-    
-public class Shuttle extends Vehicle implements DrawableItem
-{
-    private LinkedList<Location> destinations= new LinkedList<Location>();
-    private LinkedList<Passenger> passengers =new LinkedList<Passenger>();
-    private Location location;
-    private LuxCompany company;
-   // private Shuttle bus;//criando o bus
-    private Image emptyImage, passengerImage;//as imagens
 
-    public Shuttle(LuxCompany company, Location location){
+public class Shuttle extends Vehicle implements DrawableItem {
+    private Passenger passenger;
+    private Shuttle bus;// criando o bus
+    private Image emptyImage, passengerImage;// as imagens
+
+    public Shuttle(LuxCompany company, Location location) {
         super(company, location);
-        //destinations = new LinkedList<Location>();
-        //passengers = new LinkedList<Passenger>();
         emptyImage = new ImageIcon(getClass().getResource("images/bus.png")).getImage();
         passengerImage = new ImageIcon(getClass().getResource("images/driverBus.png")).getImage();
     }
 
-    public void act()
-    {
+    public void act() {
         Location target = getTargetLocation();
-        if(target != null) {
+        if (target != null) {
             Location next = getLocation().nextLocation(target);
             setLocation(next);
-            if(next.equals(target)) {
-                for (int i = 0; i < passengers.size(); i++) {
-                    if(passengers.get(i) != null) {
-                            notifyPassengerArrival(passengers.get(i)); //notificar a chegada do passageiro
-                            offloadPassenger();
-                    }else {
-                        notifyPickupArrival();
-                    }                   
-                }                
+            if (next.equals(target)) {
+                if (passenger != null) {
+                    notifyPassengerArrival(passenger); // notificar a chegada do passageiro
+                    offloadPassenger();
+                } else {
+                    notifyPickupArrival();
+                }
             }
-        }
-        else {
+        } else {
             incrementIdleCount();
         }
     }
 
-           
-    
-    public void companyLocation(Location location, LuxCompany company){
-        this.company = company;
-        this.location = location;
-        }
+    public boolean isFree() {
+        return getTargetLocation() == null && passenger == null;
+    }
 
-    public void setPickupLocation(Location location){
-       // destinations.add(location);
-        //chooseTargetLocation();
+    public void setPickupLocation(Location location) {
         setTargetLocation(location);
     }
-    
 
-    public void pickup(Passenger passenger){
-        passengers.addLast(passenger);
-        //destinations.add(passenger.getDestination());
+    public void pickup(Passenger passenger) {
+        this.passenger = passenger;
         setTargetLocation(passenger.getDestination());
-        
     }
-    
-    public void offloadPassenger(){
-        
-        passengers = null;
-        clearTargetLocation();
-    } 
 
-    @Override
-    public boolean isFree() {
-        return getTargetLocation() == null && passengers == null;
-        
+    public void offloadPassenger() {
+        passenger = null;
+        clearTargetLocation();
     }
-    
-    @Override
-    public Image getImage()
-     { 
-            if(passengers != null) {
-                return passengerImage;
-            }
-            else {
-                return emptyImage;
-            }     
+
+    public Image getImage() {
+        if (passenger != null) {
+            return passengerImage;
+        } else {
+            return emptyImage;
+        }
+    }
+
+    public String toString() {
+        return "luxCar at " + getLocation();
     }
 
 }
