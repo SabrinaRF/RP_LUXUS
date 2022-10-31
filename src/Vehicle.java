130955af817d40713a356ec7c310deffc60df109
JonesGamer18 <106637017+JonesGamer18@users.noreply.github.com>
2022-10-31 10:28:24 -0300

    Update Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,72 +1,78 @@
+import java.util.List;
 
 public abstract class Vehicle implements Actor {
-    
+	private LuxCompany company;
+	private Location location;
 
-    private LuxCompany company;
-    private Location location;
-
-    private Location targetLocation;
+	private Location targetLocation;
 	private int idleCount;
 
-    public Vehicle(LuxCompany company, Location location){
-        this.company = company;
-        this.location = location;
-    }
+	public Vehicle(LuxCompany company, Location location) {
+		this.company = company;
+		this.location = location;
+	}
 
-    public LuxCompany getCompany(){return this.company;}
-    public Location getLocation(){return location;}
+	public LuxCompany getCompany() {
+		return this.company;
+	}
 
-    
-    public void setPickupLocation(Location location){this.location = location;}
-    public abstract void pickup(Passenger passenger);
-    public abstract boolean isFree(); 
+	public Location getLocation() {
+		return location;
+	}
 
-    public void notifyPickupArrival() {
+	public void setPickupLocation(Location location) {
+		this.location = location;
+	}
+
+	public abstract void pickup(Passenger passenger);
+
+	public abstract boolean isFree();
+
+	public void notifyPickupArrival() {
 		company.arrivedAtPickup(this);
 	}
 
-    public void notifyPassengerArrival(Passenger passenger) {
-		
-		company.arrivedAtDestination(this, passenger);
+	public void notifyPassengerArrival(Passenger passengers) {
+
+		company.arrivedAtDestination(this, passengers);
 	}
 
+	public void notifyPassengerArrivalBus(List<Passenger> passengers) {
 
+		company.arrivedAtDestinationBus(this, passengers);
+	}
 
-    public void setLocation(Location location) {
-		if(location != null) {
+	public void setLocation(Location location) {
+		if (location != null) {
 			this.location = location;
-		}
-		else {
+		} else {
 			throw new NullPointerException();
 		}
-		
-		}
-		
-		public void clearTargetLocation() {
-			targetLocation = null;
-		}
-		
-		public int getIdleCount() {
-			return idleCount;
-		}
-		
-		public void incrementIdleCount() {
-			idleCount++;
-		}
-		public Location getTargetLocation() {
-			return targetLocation;
-		}
-		
-		public void setTargetLocation(Location location) {
-			if (location != null) {
-				targetLocation = location;
-			}
-			else {
-				throw new NullPointerException();
-			}
-		}
 
-    
+	}
+
+	public void clearTargetLocation() {
+		targetLocation = null;
+	}
+
+	public int getIdleCount() {
+		return idleCount;
+	}
+
+	public void incrementIdleCount() {
+		idleCount++;
+	}
+
+	public Location getTargetLocation() {
+		return targetLocation;
+	}
+
+	public void setTargetLocation(Location location) {
+		if (location != null) {
+			targetLocation = location;
+		} else {
+			throw new NullPointerException();
+		}
+	}
+
 }
-
-    
